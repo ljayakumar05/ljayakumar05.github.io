ce2169256dc37e3a732605ff595043b0a3b5e5b4
Jayakumar L <ljayakumar05@gmail.com>
2021-06-18 21:17:10 +0530

    scriptz_changes

--- javascript/01-FUNDAMENTALS-PART-1/js/slider.js
+++ javascript/01-FUNDAMENTALS-PART-1/js/slider.js
@@ -1,26 +1,115 @@
-console.log( 5 + 5 ); 
+// console.log( 5 + 5 ); 
 
-let js = "amazing";
+// let js = "amazing";
 
-console.log("jonas");
+// console.log("jonas");
 
-let PT = 3.1415;
+// let PT = 3.1415;
 
-let myFirstJob = "programmer";
-let myCurrentJob = "Teacher";
+// let myFirstJob = "programmer";
+// let myCurrentJob = "Teacher";
 
-console.log(myFirstJob);
+// console.log(myFirstJob);
 
-let myName = "jayakumar";
-console.log(myName); 
+// let myName = "jayakumar";
+// console.log(myName); 
 
-myName = "JAI"; 
-console.log(myName);
-console.log(typeof myName); 
+// myName = "JAI"; 
+// console.log(myName);
+// console.log(typeof myName); 
+ 
+// let firstName = "Jayakumar";
 
-let firstName = "Jayakumar";
+// console.log(typeof myName);
 
-console.log(typeof myName);
+// console.log(typeof firstName);
+// console.log(typeof firstName);
 
-console.log(typeof firstName);
-console.log(typeof firstName);
+// var lastName = "JayakumarL";
+// lastName = "KumarL";
+// console.log(lastName);
+
+//operaters
+//Math operators
+// const now = 2021;
+// const ageJai = now - 1992;
+// const agejothi = now - 1993;
+// console.log(ageJai, agejothi);  
+
+// console.log(ageJai * 2, ageJai / 10, 2 ** 3);
+
+// const firstName = "Jayakumar";
+// const lastName = "Loganathan";
+// console.log(firstName +" "+ lastName);
+
+// //Assignement operators
+// let x = 10 + 5; //15
+// x += 10; //x = x + 10 = 25
+// x *= 4; // x = x * 4 = 100
+// x++; // X = X + 1
+// x--;
+// x--;
+// console.log(x);
+
+// //Comparison Operatiors
+
+// console.log(ageJai < agejothi);
+
+
+// const massMark = 78;
+// const heightMark = 1.69;
+
+// const massJohn = 92;
+// const heightJohn = 1.95;
+
+// const massMark = 95;
+// const heightMark = 1.88;
+
+// const massJohn = 85;
+// const heightJohn = 1.76;
+
+// const BMIMark = massMark / heightMark ** 2;
+// const BMIJohn = massJohn / (heightJohn * heightJohn);
+// const markHigherBMI = BMIMark > BMIJohn;
+// console.log(BMIMark, BMIJohn, markHigherBMI);
+
+// String **
+
+// const firstName = 'Jai';
+// const job = 'Teacher';
+// const birthYear = 1992;
+// const year = 2037;
+
+// const jai = "I'm " + firstName + ', a ' + (year - birthYear) + 'years old ' + job + '!';
+
+// console.log(jai);
+
+// const jail = `I'm ${firstName}, a ${year - birthYear} year old ${job}.`;
+
+// console.log(jail); 
+
+// console.log(`hello 
+// world`); 
+
+// if elst statement **
+
+const firstName = 'Jayakumar';
+const age = 19;
+
+const isoldEnough = age >=18 ;
+
+if(isoldEnough) {
+    console.log('jai can start driving license 😀');
+}else{
+    console.log('sorry Jai 😌');
+} 
+
+const birthYear = 1992;
+
+let century;
+if (birthYear <= 2000) {
+    century = 20;
+}else{
+    century = 21;
+}
+console.log(century);
