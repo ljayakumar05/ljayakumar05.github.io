a6239cf2a65db7cbfc0c7120f1dabfdb81e6ec36
Jayakumar L <ljayakumar05@gmail.com>
2021-06-23 16:26:12 +0530

    function inline

--- javascript/02-FUNDAMENTALS-PART-2/script.js
+++ javascript/02-FUNDAMENTALS-PART-2/script.js
@@ -1,1 +1,35 @@
+//   console.log('Hello wold 2');
+
+//   function fruitProcessor(apple, orange){
+//       console.log(apple, orange);
+//       const juice = `Juice with ${apple} apple and ${orange} orange.`;
+//       return juice; 
+//   }
+
+// const appleJuice = fruitProcessor(50, 55);
+// console.log(appleJuice);
+
+//   function calAge1(birthYear){
+//       return 2021 - birthYear;
+//   }
+
+//   const age = calAge1(1992);
+//   console.log(`My age is ${age}.`);
+
+  //Function inside a function: Data flow
+
+  const cutPieces = function (fruit) {
+      return fruit * 4;
+  }
+
+  const fruitProcessor = function (apples, oranges) {
+    
+    const applePieces = cutPieces(apples);
+    const orangePieces = cutPieces(oranges);
+
+    const juice = `Juice with ${applePieces} pieces of apple and ${orangePieces} pieces of orange.`;
+    return juice;
+
+  }
   
+  console.log(fruitProcessor(2,3));
