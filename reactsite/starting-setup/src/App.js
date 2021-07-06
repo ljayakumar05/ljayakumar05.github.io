717a6542fe02f7801c4af5adb138065618a5516b
Jayakumar L <ljayakumar05@gmail.com>
2021-07-06 10:58:27 +0530

    component

---
+++ reactsite/starting-setup/src/App.js
@@ -0,0 +1,14 @@
+import ExpenseItem from "./components/ExpenseItem.js";
+
+function App() {
+  return (
+    <div>
+      <h2>Let's get started!</h2>
+      <center>
+        <ExpenseItem></ExpenseItem>
+      </center>
+    </div>
+  );
+}
+
+export default App;
